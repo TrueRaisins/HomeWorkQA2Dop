c74c49ece77b88e22c057171c23cffdced5ac34a
TrueRaisins <idanik.ishot@gmail.com>
2023-11-02 00:10:02 +0300

    Initial commit

---
+++ src/main/java/Primitives.java
@@ -0,0 +1,16 @@
+public class Primitives {
+  public static void main (String[] args) {
+    // Целочисленные типы
+    byte TestByte = -128;
+    short TestShort =-32768;
+    int TestInt = -2147483648;
+    long TestLong = -922372036854775808L;
+    // Символы
+    char TestChar = 'f';
+    // Типы с плавающей точкой
+    float TestFloat = 3.4028235E38F;
+    double TestDouble = 1.7976931348623157E308;
+    // Логический тип
+    boolean TestBoolean = true;
+  }
+}
