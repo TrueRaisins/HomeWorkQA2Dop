8b8e0a34e7c2e27562f751a8715b7d594634de1e
TrueRaisins <idanik.ishot@gmail.com>
2023-11-02 00:20:25 +0300

    initial commit

---
+++ src/main/java/Wrappers.java
@@ -0,0 +1,12 @@
+public class Wrappers {
+    public static void main (String[] args){
+        Integer i = new Integer(233);
+        Short s = new Short(17);
+        Long l = new Long(13245213L);
+        Byte b = new Byte(12);
+        Float f = new Float(1.2345);
+        Double d = new Double(0.1234112);
+        Character c = new Character('f');
+        Boolean b1 = new Boolean(false);
+    }
+}
